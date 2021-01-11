c78320ecf91cecb9d0fd107314f38395524ffe1b
Chris <cmh@chrishumphries.net>
2021-01-10 20:46:23 -0500

    added baker class and tests

--- baker.rb
+++ baker.rb
@@ -1,10 +1,17 @@
 class Baker
     
-    attr_reader :baker_first_name
+    attr_reader :baker_first_name, :baker_last_name, :baker_zip_code
 
-    def initialize baker_first_name
+    def initialize baker_first_name, baker_last_name, baker_zip_code
         #instance variables
         @baker_first_name = baker_first_name
+        @baker_last_name = baker_last_name
+        @baker_zip_code = baker_zip_code
     end
+
+    def createUsername
+
+    end
+    
 
 end
--- baker_spec.rb
+++ baker_spec.rb
@@ -4,13 +4,34 @@ RSpec.describe Baker do
 
     before do
         baker_first_name = "Christine"
+        baker_last_name = "Humphries"
+        baker_zip_code = "43204"
 
-        @baker = Baker.new baker_first_name
+        @baker = Baker.new baker_first_name, baker_last_name, baker_zip_code
     end
     
-    it 'should respond to first name' do
-       expect(@baker).to respond_to(:baker_first_name)
+    describe 'add baker' do 
+        it 'should respond to first name' do
+            expect(@baker).to respond_to(:baker_first_name)
+        end
+
+        it 'should respond to last name' do
+            expect(@baker).to respond_to(:baker_last_name)
+        end
+
+        it 'baker first name should be Christine' do
+            expect(@baker.baker_first_name).to eq("Christine")
+        end
+
+        it 'baker last name should be Humphries' do
+            expect(@baker.baker_last_name).to eq("Humphries")
+        end
+
+        it 'baker zipcode should be 43204' do
+            expect(@baker.baker_zip_code).to eq("43204")
+        end
     end
+    
     
 
 
