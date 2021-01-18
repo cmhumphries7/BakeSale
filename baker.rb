3e0d9a8f52eac8017becfdcaa0795875a4bb79d8
Chris <cmh@chrishumphries.net>
2021-01-18 17:11:22 -0500

    delivery preference

--- baker.rb
+++ baker.rb
@@ -1,17 +1,46 @@
 class Baker
     
     attr_reader :baker_first_name, :baker_last_name, :baker_zip_code
+    attr_accessor :baker_delivery_preference
 
-    def initialize baker_first_name, baker_last_name, baker_zip_code
+    def initialize baker_first_name, baker_last_name, baker_zip_code, baker_delivery_preference
         #instance variables
         @baker_first_name = baker_first_name
         @baker_last_name = baker_last_name
         @baker_zip_code = baker_zip_code
+        @baker_delivery_preference = baker_delivery_preference
     end
 
-    def createUsername
+    def set_delivery_preference(delivery_input)
+        delivery_options = ["Pickup", "Delivery", "Open to Either"]
+        puts "Which delivery method works for you?"
+        puts "press 1 for pickup, 2 for delivery, and 3 for either"
+        delivery_input = gets
 
+
+        if (delivery_input == 1)
+            #delivery_preference = @baker.set_delivery_preference (delivery_input)
+            @baker_delivery_preference = delivery_options[0]
+            #sets baker preference to pickup
+        end
+
+        if (delivery_input == 2)
+            #sets baker preference to delivery
+        end
+
+        if (delivery_input == 3)
+            #sets baker preference to either
+        end
     end
+
+    def to_s
+        output = ""
+        output + @delivery_preference.to_s
+    end
+
+
+
+    
     
 
 end
--- baker_spec.rb
+++ baker_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe Baker do
         baker_first_name = "Christine"
         baker_last_name = "Humphries"
         baker_zip_code = "43204"
+        baker_delivery_preference = ""
 
-        @baker = Baker.new baker_first_name, baker_last_name, baker_zip_code
+
+        @baker = Baker.new baker_first_name, baker_last_name, baker_zip_code, baker_delivery_preference
     end
     
     describe 'add baker' do 
@@ -31,6 +33,36 @@ RSpec.describe Baker do
             expect(@baker.baker_zip_code).to eq("43204")
         end
     end
+
+    describe 'set delivery option' do
+        it 'should set baker preference to pickup' do
+            delivery_options = ["Pickup", "Delivery", "Open to Either"]
+            delivery_input = 1
+            baker_delivery_preference = @baker.set_delivery_preference(delivery_input)
+           
+           
+            #delivery_preference = delivery_preference.to_s
+
+            expect("#{@baker.baker_delivery_preference}").to eq("Pickup")
+        end
+
+        it 'should set baker preference to delivery' do
+            delivery_options = ["Pickup", "Delivery", "Open to Either"]
+            delivery_input = (delivery_options[1])
+            baker_delivery_preference = @baker.set_delivery_preference(delivery_input)
+            #delivery_preference = delivery_preference.to_s
+
+            expect("#{@baker.baker_delivery_preference}").to eq("Delivery")
+        end
+
+        it 'should set baker preference to open to either' do
+        end
+
+
+
+
+    end
+
     
     
 
